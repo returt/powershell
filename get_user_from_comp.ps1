d6e4b7c64c58db6bc4f72608ba81f8a8563faa5f
returt <48471920+returt@users.noreply.github.com>
2019-10-29 13:05:22 +0300

    Update get_user_from_comp.ps1

--- get_user_from_comp.ps1
+++ get_user_from_comp.ps1
@@ -1,34 +1,32 @@
-# скрипт определяет, какой юзер залогинился на компьютере
+# script determines which user loged on the computer
 import-module activedirectory
-# область поиска
+# search arrea
 $OU="OU=,OU=,OU=,DC=,DC="
-# указываем Домен
-$domain=''
-# указываем путь к отчету
+# specify the path to the report
 $reportpatch="c:\temp\audit.csv"
 if (Test-Path -Path $reportPatch) {Remove-Item $reportPatch}
-# отбираем ПК по фильтру
+# select PC by filter
 Get-ADComputer -Filter * -SearchBase $OU  |
 ForEach-Object {
-# переводим в переменную только имя ПК
+# transfer only netbios name of the PC into a variable
 $computer=($_).Name
 if ((Test-Connection -computer $computer -count 2 -quiet) -eq $True)
 {
-# Узнаем кто работает на данном ПК и удаляем приставку с названием домена
+# find out who is working on this PC and remove domain_name\
 $sam=(get-wmiobject win32_computerSystem -computerName $computer).username.split("\")[1]
 if ($sam -ne $null)
 {
-# Находим пользователя в AD по samaccountname и переводим в вывод Имя Фамилия
+# find the user in AD by samaccountname and put them in First Name Last Name
 $dn=(Get-ADUser -identity $sam).Name
 if ($dn -ne $null)
 {
-# выводим полученный результат - ПК - Имя Фамилия
+# display the result: PC - First name Last name
 echo "$computer - $dn" >> $reportpatch
 }
 }
 else 
 {
-# записываем "проблемные" компы
+# writing "problem" computers
 echo $computer >> c:\temp\bad_comp.txt
 }
 }
